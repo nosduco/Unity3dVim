24c2a3321fe425e0f5a4d661cf16442b356b1d19
Tony Duco <nosduco@gmail.com>
2017-05-19 17:07:21 -0400

    Fixed inconsistency in bash code

--- Assets/Plugins/Editor/Vim/VimRunner.sh
+++ Assets/Plugins/Editor/Vim/VimRunner.sh
@@ -4,7 +4,7 @@ touch test
 #[Configuration]
 TERM="xfce4-terminal"
 
-if [ ! -z `vim --serverlist | grep UNITY_SCRIPT_EDITOR` ]; then
+if [ ! -z `vim --serverlist | grep Unity3d` ]; then
     vim --servername Unity3d --remote-silent +$2 $1
 else
     $TERM -e "vim --servername Unity3d --remote-silent +$2 $1"
